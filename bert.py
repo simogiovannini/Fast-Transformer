c16473ed9c379264849385275f33e17e3cf38381
Simone Giovannini <sg7107982@localhost.dinfo.unifi.it>
2023-12-27 16:01:56 +0100

    Removed useless parameters

--- bert.py
+++ bert.py
@@ -5,15 +5,10 @@ from tokenizer import load_tokenizer
 TOKENIZER_BATCH_SIZE = 256  # Batch-size to train the tokenizer on
 TOKENIZER_VOCABULARY = 25000  # Total number of unique subwords the tokenizer can have
 
-BLOCK_SIZE = 128  # Maximum number of tokens in an input sample
-NSP_PROB = 0.50  # Probability that the next sentence is the actual next sentence in NSP
-SHORT_SEQ_PROB = 0.1  # Probability of generating shorter sequences to minimize the mismatch between pretraining and fine-tuning.
 MAX_LENGTH = 512  # Maximum number of tokens in an input sample after padding
 
-MLM_PROB = 0.2  # Probability with which tokens are masked in MLM
-
 TRAIN_BATCH_SIZE = 2  # Batch-size for pretraining the model on
-MAX_EPOCHS = 1  # Maximum number of epochs to train the model for
+TRAIN_EPOCHS = 1  # Maximum number of epochs to train the model for
 LEARNING_RATE = 1e-4  # Learning rate for training the model
 
 tokenizer = load_tokenizer(TOKENIZER_BATCH_SIZE, TOKENIZER_VOCABULARY)
